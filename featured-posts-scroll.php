6efb94e933e88748266c5484a76612f97bedb557
Chaser324 <Chaser324@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-26 12:56:22 +0000

    git-svn-id: https://plugins.svn.wordpress.org/featured-posts-scroll/branches/varsize@415329 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- featured-posts-scroll.php
+++ featured-posts-scroll.php
@@ -3,7 +3,7 @@
 Plugin Name: Featured Posts Scroll
 Plugin URI: http://chasepettit.com
 Description: A basic javascript based scrolling display of post titles and thumbnails.
-Version: 1.15
+Version: 1.16
 Author: Chaser324
 Author URI: http://chasepettit.com
 License: GNU GPL2
